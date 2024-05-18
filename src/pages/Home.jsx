cf61c3e3d4434df01c6244cf33e5f3a7047a550a
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-05-18 10:48:49 +0530

    link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { RiArrowRightSLine } from "react-icons/ri"; // Importing the right arrow icon
 import { RiMailLine, RiPhoneLine, RiMapPinLine } from "react-icons/ri";
 const Home = () => {
